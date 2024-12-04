7d3066d69cdd3bd7c1c998bc4b8b98e51b6104f9
Marco Dugatto <dughi.boogie@gmail.com>
2024-12-04 21:48:12 +0100

    Added custom colors, changed default bg color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,15 @@ export default {
   ],
   theme: {
     extend: {
+      width: {
+        "200": "50rem",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "tea-green": "#C9F2C7",
+        "space-cadet": "#3B335C",
+        "rose-quartz": "#BCABAE",
       },
     },
   },
