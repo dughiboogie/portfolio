68cfc55acb0451c05660a3a938f3542984f6da4e
Marco Dugatto <dughi.boogie@gmail.com>
2024-12-13 20:48:39 +0100

    Home page - First contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="md:mt-28 mt-8 relative z-20">
         <Navbar></Navbar>
 
-        <div className="flex flex-col items-center text-center px-4 md:px-10">
+        <div className="flex flex-col items-center text-center px-4 md:px-10 mt-4 md:mt-8 md:mb-8">
           <motion.div
             initial={{
               opacity: 0,
@@ -31,18 +31,17 @@ export default function Home() {
             }}
             className="text-tea-green"
           >
-            <p className="font-roboto font-extrabold text-4xl md:text-6xl">
-              Welcome to My Portfolio
+            <p className="font-roboto font-extrabold text-4xl md:text-7xl">
+              Marco Dugatto - DughiBoogie
             </p>
-            <p className="max-w-3xl font-roboto text-base md:text-2xl">
-              Hi there! I am Marco Dugatto (Dughi for short), a 29 years old guy
-              with a constant curiosity for what life has to offer. I am also
-              italian, so if my English is bad I humbly beg your pardon in
-              spaghetti. I have way too many passions and way too little time to
-              follow them all, but isn&apos;t that the story of everyone&apos;s
-              life? Fortunately for me I have learned to focus on just a bunch
-              of them, so since January 2021 I&apos;ve been learning to make
-              video games.
+            <p className="font-roboto font-extrabold text-3xl md:text-5xl mx-4">
+              Game Developer | Nature Explorer | Music Aficionado
+            </p>
+            <p className="max-w-md md:max-w-3xl font-roboto text-base text-left md:text-2xl inline-block">
+              It's a me, Marco!<br></br>
+              I’m a game developer with a passion for engaging worlds and
+              experiences.<br></br> I dream of crafting such adventures to make
+              people feel the same passion that drives me.
             </p>
           </motion.div>
         </div>
@@ -68,33 +67,54 @@ export default function Home() {
           >
             <div
               id="Spotlight"
-              className="bg-space-cadet/30 p-6 rounded-2xl border-gray-400 border-2 max-w-sm md:max-w-4xl mx-8"
+              className="font-roboto bg-space-cadet/30 p-6 rounded-2xl border-gray-400 border-2 max-w-sm md:max-w-4xl mx-8"
             >
-              <p>Explore my work and projects.</p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Pellentesque viverra nisi diam, in pharetra felis pretium quis.
-                Cras mi mi, lobortis eu erat vel, dignissim mollis purus. Donec
-                tincidunt feugiat risus. Cras in leo sit amet orci dictum
-                pellentesque. Vestibulum dapibus facilisis diam, vel
-                pellentesque sem viverra at. Nam accumsan rhoncus lacus, sit
-                amet interdum arcu varius sed. Phasellus at orci ut lectus
-                rhoncus luctus. Nullam ut eros mollis, rutrum nunc vitae,
-                imperdiet nisl.
+              <p className="font-bold text-2xl md:text-3xl text-center">
+                Latest project
               </p>
-              <p>
-                Nunc ullamcorper finibus dui eget imperdiet. Donec lobortis
-                tincidunt elit. Nullam mattis quam sit amet feugiat semper. Sed
-                volutpat, velit congue blandit faucibus, tellus nisl porta
-                augue, eget blandit augue dui vel mauris. Vestibulum venenatis
-                iaculis ante consequat pretium. Proin nec lectus sem. Maecenas
-                id augue at justo maximus lobortis. Nullam sagittis nunc eu
-                tellus dictum, sed gravida mi volutpat.
+              <p className="font-medium text-2xl md:text-3xl -mt-4 text-center md:text-left">
+                Things Better Left Unknown
               </p>
-              <div className=" self-center">
+              <div className="flex flex-col md:flex-row gap-4 items-center -mt-4">
+                {/* Image Section */}
+                <div className="flex-shrink-0">
+                  <Link href="https://deeprestgames.itch.io/things-better-left-unknown">
+                    <img
+                      className="w-full md:w-72 self-center rounded-lg"
+                      src="pictures/TBLU_1.png"
+                      alt="Project Thumbnail"
+                    />
+                  </Link>
+                </div>
+                {/* First Paragraph Section */}
+                <div>
+                  <p className="font-medium self-center italic text-md mb-0">
+                    Delve into the dark secrets of alchemy. Explore a mysterious
+                    cabin in the woods. Decipher glyphs, codes, and poems.
+                    Combine unusual ingredients through alchemical processes,
+                    and uncover things that would be better left unknown.
+                  </p>
+                </div>
+              </div>
+              {/* Second Paragraph Section */}
+              <div className="mt-4">
+                <p>
+                  I made this game with some friends for the Pirate Software
+                  Game Jam 15. The development process has been a lot of fun,
+                  and I learned a ton of stuff working on it.<br></br>
+                  The game is made in Godot, and it's fully playable online.
+                  Check it out on itch.io!
+                </p>
+              </div>
+              <div className="mt-4 mb-1 flex justify-center md:gap-8 gap-4 max-w-full">
+                <Link href="https://deeprestgames.itch.io/things-better-left-unknown">
+                  <button className="px-2 py-2 w-full max-w-xs md:w-48 bg-rose-quartz text-black font-bold rounded-lg shadow-2xl">
+                    Go to itch.io game page
+                  </button>
+                </Link>
                 <Link href="/projects">
-                  <button className="mt-4 px-2 py-2 bg-rose-quartz text-black font-bold rounded-lg shadow-2xl">
-                    See My Projects
+                  <button className="px-2 py-2 w-full max-w-xs md:w-48 bg-rose-quartz text-black font-bold rounded-lg shadow-2xl">
+                    Go to all projects
                   </button>
                 </Link>
               </div>
