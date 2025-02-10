748c2e2fbc08222b4880bac42cb8db67db424c6f
Marco Dugatto <dughi.boogie@gmail.com>
2025-02-10 17:59:38 +0100

    Made portfolio page unique - games showcase only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { motion } from "framer-motion";
 import Link from "next/link";
-import Navbar from "./components/Navbar";
 import BlurredMountainsBackground from "./components/blurredMountainsBackground";
 import SpotlightCard_HomePage from "./components/spotlightCard_HomePage";
 
@@ -12,8 +11,6 @@ export default function Home() {
       <BlurredMountainsBackground />
 
       <div className="md:mt-28 mt-8 relative z-20">
-        <Navbar></Navbar>
-
         <div className="flex flex-col items-center text-center px-4 md:px-10 mt-4 md:mt-8 md:mb-8">
           {/* Header */}
           <motion.div
@@ -41,61 +38,12 @@ export default function Home() {
               I’m a game developer with a passion for engaging worlds and
               meaningful experiences.<br></br> I dream of crafting games that I
               would love to play, and that make the players feel the same
-              passion.
+              passion.<br></br>
+              <br></br>
+              In this page are collected (almost) all the games I worked on so
+              far.<br></br>
+              More to come!
             </p>
-
-            <div className="my-8 flex flex-col items-center justify-center md:gap-8 gap-4 max-w-full">
-              <Link className="flex text-base" href="/aboutMe">
-                <motion.div
-                  className="md:px-4 px-4 md:py-4 py-2 rounded-xl border border-gray-400 flex items-center gap-2"
-                  whileTap={{ scale: 0.9 }}
-                  initial={{
-                    backgroundColor: "#0000008D",
-                  }}
-                  whileHover={{
-                    backgroundColor: "#22222299",
-                  }}
-                  animate={{
-                    backgroundColor: "#0000008D",
-                  }}
-                  transition={{ type: "tween", ease: "easeOut" }}
-                >
-                  <div className="font-semibold flex text-white">
-                    Learn more about me
-                  </div>
-                  <img
-                    src="/icons/arrow-right.svg"
-                    alt={`Arrow icon`}
-                    className="hidden md:w-5 md:h-5 md:block"
-                  />
-                </motion.div>
-              </Link>
-              <Link className="flex text-base" href="/games">
-                <motion.div
-                  className="md:px-4 px-4 md:py-4 py-2 rounded-xl border border-gray-400 flex items-center gap-2"
-                  whileTap={{ scale: 0.9 }}
-                  initial={{
-                    backgroundColor: "#0000008D",
-                  }}
-                  whileHover={{
-                    backgroundColor: "#22222299",
-                  }}
-                  animate={{
-                    backgroundColor: "#0000008D",
-                  }}
-                  transition={{ type: "tween", ease: "easeOut" }}
-                >
-                  <div className="font-semibold flex text-white">
-                    Explore my games
-                  </div>
-                  <img
-                    src="/icons/arrow-right.svg"
-                    alt={`Arrow icon`}
-                    className="hidden md:w-5 md:h-5 md:block"
-                  />
-                </motion.div>
-              </Link>
-            </div>
           </motion.div>
         </div>
 
@@ -120,8 +68,8 @@ export default function Home() {
           >
             {/* TBLOU */}
             <SpotlightCard_HomePage
-              spotlightHeader="LATEST GAME"
-              spotlightTitle="Things Better Left Unknown"
+              spotlightHeader="Things Better Left Unknown"
+              spotlightTitle=""
               spotlightContent=<div>
                 <p className="font-bold text-2xl md:text-3xl text-center"></p>
                 <p className="font-medium text-2xl md:text-3xl -mt-4 text-center md:text-left"></p>
@@ -132,7 +80,6 @@ export default function Home() {
                       className="absolute w-full h-full rounded-lg"
                       src="https://www.youtube.com/embed/p160z1ScYEk?si=eSDh3ExWkMcMmuF5"
                       title="YouTube video player"
-                      frameBorder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                       allowFullScreen
                     ></iframe>
@@ -148,8 +95,30 @@ export default function Home() {
                     </p>
                   </div>
                 </div>
+                {/* Second Paragraph Section */}
+                <div className="mt-4">
+                  <p>
+                    I made this game with some friends for the Pirate Software
+                    Game Jam 15.<br></br> There were 5 people working on the
+                    game so the project management side has been a bit hectic,
+                    but I learned a lot about pivoting according to the
+                    deadlines to meet everyone's needs and complete the project
+                    in time.<br></br>
+                    This being said, it was a ton of fun working on this.
+                    <br></br>
+                    <br></br>
+                    The game is made in Godot, and it's fully playable online.
+                    <br></br>
+                    Check it out on itch.io!
+                  </p>
+                </div>
                 <div className="mt-4 mb-1 flex justify-center md:gap-8 gap-4 max-w-full">
-                  <Link className="flex text-base" href="/games">
+                  <Link
+                    className="flex text-base"
+                    href="https://deeprestgames.itch.io/things-better-left-unknown"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
                     <motion.div
                       className="md:px-4 px-4 md:py-4 py-2 rounded-xl border border-gray-400 flex items-center gap-2"
                       whileTap={{ scale: 0.9 }}
@@ -165,77 +134,157 @@ export default function Home() {
                       transition={{ type: "tween", ease: "easeOut" }}
                     >
                       <div className="font-semibold flex text-white">
-                        Explore my games
+                        Go to game page
                       </div>
-                      <img
-                        src="/icons/arrow-right.svg"
-                        alt={`Arrow icon`}
-                        className="hidden md:w-5 md:h-5 md:block"
-                      />
                     </motion.div>
                   </Link>
                 </div>
               </div>
             />
 
-            {/* WHAT'S GOING ON */}
+            {/* Pizza Time */}
             <SpotlightCard_HomePage
-              spotlightHeader={
-                <Link
-                  className="font-bold text-3xl md:text-4xl text-center"
-                  href="https://media.tenor.com/uJm_ZTk5WGMAAAAM/heman-hey.gif"
-                >
-                  WHAT'S GOING ON
-                </Link>
-              }
+              spotlightHeader="Pizza Time"
               spotlightTitle=""
               spotlightContent=<div>
                 <p className="font-bold text-2xl md:text-3xl text-center"></p>
                 <p className="font-medium text-2xl md:text-3xl -mt-4 text-center md:text-left"></p>
                 <div className="flex flex-col gap-4 items-center -mt-4">
+                  {/* Image Section */}
+                  <div className="relative w-full pb-[56.25%] flex">
+                    <iframe
+                      className="absolute w-full h-full rounded-lg"
+                      src="https://www.youtube.com/embed/DrjSL8mQMjk?si=xPvPJ-kbiS9dH_JM"
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
                   {/* First Paragraph Section */}
                   <div>
-                    <p className="font-medium italic self-center text-md mb-0 mt-2">
-                      I'm currently in Italy, working as a System Administrator
-                      for a manufacturing company.<br></br>I recently started
-                      playing live with a band after almost 6 years from the
-                      last gig.<br></br>
-                      In 2024 I started making games with a friend from Munich,
-                      and we ended up developing four games (plus a couple
-                      unfinished ones).<br></br>
-                      In 2024 I also picked up climbing.<br></br>
-                      In 2023 I came back to Italy from Hamburg on a bike, on a
-                      month long trip.<br></br>
-                      That was the last spoiler.<br></br>
+                    <p className="font-medium self-center italic text-md mb-0 mt-2">
+                      Pizza Time is a short 3D Puzzle-Platformer adventure in
+                      which you explore an architectural dreamscape, meet
+                      eccentric characters and throw pizzas to solve puzzles.
+                      <br></br>
+                      Climb your way to the top and dig deeper in yourself.
                     </p>
                   </div>
                 </div>
-                <div className="mt-4 mb-1 flex justify-center md:gap-8 gap-4 max-w-full">
-                  <Link className="flex text-base" href="/aboutMe">
-                    <motion.div
-                      className="md:px-4 px-4 md:py-4 py-2 rounded-xl border border-gray-400 flex items-center gap-2"
-                      whileTap={{ scale: 0.9 }}
-                      initial={{
-                        backgroundColor: "#0000008D",
-                      }}
-                      whileHover={{
-                        backgroundColor: "#22222299",
-                      }}
-                      animate={{
-                        backgroundColor: "#0000008D",
-                      }}
-                      transition={{ type: "tween", ease: "easeOut" }}
+                {/* Second Paragraph Section */}
+                <div className="mt-4">
+                  <p>
+                    I worked on this game for three months with a friend, and
+                    it's been a wild ride.<br></br>
+                    It started as a fun project that we decided to do right
+                    after our first game jam together, and it became a massive
+                    puzzle game with some wacky mechanics and a somewhat deep
+                    narrative.<br></br>
+                    <br></br>
+                    During this project I learned the real meaning of{" "}
+                    <b>scope creep</b>. New ideas were continuously bouncing
+                    around and discussions were always stimulating. We had to
+                    put real effort in saying "let's stop here and publish
+                    this".
+                  </p>
+                  <div className="mt-4 mb-1 flex justify-center md:gap-8 gap-4 max-w-full">
+                    <Link
+                      className="flex text-base"
+                      href="https://deeprestgames.itch.io/pizza-time"
+                      rel="noopener noreferrer"
+                      target="_blank"
                     >
-                      <div className="font-semibold flex text-white">
-                        Learn more about me
-                      </div>
-                      <img
-                        src="/icons/arrow-right.svg"
-                        alt={`Arrow icon`}
-                        className="hidden md:w-5 md:h-5 md:block"
-                      />
-                    </motion.div>
-                  </Link>
+                      <motion.div
+                        className="md:px-4 px-4 md:py-4 py-2 rounded-xl border border-gray-400 flex items-center gap-2"
+                        whileTap={{ scale: 0.9 }}
+                        initial={{
+                          backgroundColor: "#0000008D",
+                        }}
+                        whileHover={{
+                          backgroundColor: "#22222299",
+                        }}
+                        animate={{
+                          backgroundColor: "#0000008D",
+                        }}
+                        transition={{ type: "tween", ease: "easeOut" }}
+                      >
+                        <div className="font-semibold flex text-white">
+                          Go to game page
+                        </div>
+                      </motion.div>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            />
+
+            {/* Hell o' Borus */}
+            <SpotlightCard_HomePage
+              spotlightHeader="Pizza Time"
+              spotlightTitle=""
+              spotlightContent=<div>
+                <p className="font-bold text-2xl md:text-3xl text-center"></p>
+                <p className="font-medium text-2xl md:text-3xl -mt-4 text-center md:text-left"></p>
+                <div className="flex flex-col gap-4 items-center -mt-4">
+                  {/* Image Section */}
+                  <img
+                    className="relative md:w-4/6 w-full flex"
+                    src="/HelloBorusCover.png"
+                  ></img>
+                  {/* First Paragraph Section */}
+                  <div>
+                    <p className="font-medium self-center italic text-md mb-0 mt-2">
+                      Winter is almost over. An unknown warmth is starting to
+                      spread. Coming change is inevitable. Spring is here!
+                      <br></br> Hell o’ Borus is a 2D endless runner game where
+                      the player controls Borus, a tiny helleborus seed that
+                      awakens after being lethargic during the winter.
+                    </p>
+                  </div>
+                </div>
+                {/* Second Paragraph Section */}
+                <div className="mt-4">
+                  <p>
+                    I worked on this game for three months with a friend, and
+                    it's been a wild ride.<br></br>
+                    It started as a fun project that we decided to do right
+                    after our first game jam together, and it became a massive
+                    puzzle game with some wacky mechanics and a somewhat deep
+                    narrative.<br></br>
+                    <br></br>
+                    During this project I learned the real meaning of{" "}
+                    <b>scope creep</b>. New ideas were continuously bouncing
+                    around and discussions were always stimulating. We had to
+                    put real effort in saying "let's stop here and publish
+                    this".
+                  </p>
+                  <div className="mt-4 mb-1 flex justify-center md:gap-8 gap-4 max-w-full">
+                    <Link
+                      className="flex text-base"
+                      href="https://deeprestgames.itch.io/pizza-time"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <motion.div
+                        className="md:px-4 px-4 md:py-4 py-2 rounded-xl border border-gray-400 flex items-center gap-2"
+                        whileTap={{ scale: 0.9 }}
+                        initial={{
+                          backgroundColor: "#0000008D",
+                        }}
+                        whileHover={{
+                          backgroundColor: "#22222299",
+                        }}
+                        animate={{
+                          backgroundColor: "#0000008D",
+                        }}
+                        transition={{ type: "tween", ease: "easeOut" }}
+                      >
+                        <div className="font-semibold flex text-white">
+                          Go to game page
+                        </div>
+                      </motion.div>
+                    </Link>
+                  </div>
                 </div>
               </div>
             />
